dbd6a26a8560cb24e367626fa2a3da11957cd6bd
squall <squall@squall-PC>
2014-06-02 16:35:23 -0400

    complete W1C
    - add login and main html pages
    - add login function
    - add app view and user model

--- Jamii/views.py
+++ Jamii/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+from django.contrib import auth
+
+from models import JamiiIntroduction
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
 
 def main(request):
-    return HttpResponse('Hello')
+    if request.user.is_authenticated():
+        return render(request, 'jamii.html', locals())
+    else:
+        login = True
+        loginform = LoginForm()
+        intro = JamiiIntroduction.objects.filter(id=1)
+        if intro.count() > 0:
+            introduction = intro[0].intro   
+        if request.method=="POST":
+            username = request.POST.get('username','')
+            password = request.POST.get('password','')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None and user.is_active:
+                auth.login(request, user)
+            else:
+                is_invalid = True
+        return render(request, 'login.html', locals())
---
+++ static/js/view/app.js
@@ -0,0 +1,15 @@
+var app = app || {};
+
+app.appView = Backbone.View.extend({
+	el: '#app',
+	initialize:function(){
+		app.user = new app.UserModel();
+		//this.listenTo(this.collection, "change", this.render);
+		this.render();
+	},
+	render:function(){
+		this.$el.html(app.user.get('name'));
+	},
+
+});
+
